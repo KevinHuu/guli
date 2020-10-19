78f59e02a2fed11b81b66c54815d4fbf8e11d7b4
hukaiwen <403817740@qq.com>
2020-10-19 14:54:48 +0800

    讲师controller修改

--- service/service_edu/src/main/java/com/atguigu/eduService/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduService/controller/EduTeacherController.java
@@ -2,11 +2,18 @@ package com.atguigu.eduService.controller;
 
 
 import com.atguigu.eduService.entity.EduTeacher;
+import com.atguigu.eduService.entity.vo.TeacherQuery;
 import com.atguigu.eduService.service.EduTeacherService;
+import com.atguigu.serviceBase.exceptionHandler.GuliException;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import com.atguigu.commonUtils.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -24,14 +31,83 @@ public class EduTeacherController {
     private EduTeacherService teacherService;
 
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher(){
-        return teacherService.list(null);
+    public R findAllTeacher(){
+        List<EduTeacher> list = teacherService.list(null);
+        try {
+            int i=10/0;
+        }catch (Exception e){
+            throw new GuliException(525,"执行了GuliException异常处理");
+        }
+        return R.ok().data("items",list);
     }
 
     @ApiOperation("逻辑删除讲师")
-    @PostMapping("{id}")
-    public boolean removeTeacherById(@PathVariable String id){
-        return teacherService.removeById(id);
+    @PostMapping("deleteTeacher/{id}")
+    public R removeTeacherById(@PathVariable String id){
+        boolean b = teacherService.removeById(id);
+        if(b) return R.ok();
+        else return R.error();
+    }
+
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,
+                             @PathVariable long limit){
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+        teacherService.page(pageTeacher,null);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("total",total);
+        map.put("rows",records);
+        return R.ok().data(map);
+    }
+
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable long current,
+                                  @PathVariable long limit,
+                                  @RequestBody(required = false) TeacherQuery teacherQuery){
+        Page<EduTeacher> page = new Page<>(current, limit);
+        QueryWrapper<EduTeacher> queryWrapper = new QueryWrapper<>();
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        if(!StringUtils.isEmpty(name)){
+            queryWrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)){
+            queryWrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(begin)){
+            queryWrapper.ge("gmt_create",begin);
+        }
+        if(!StringUtils.isEmpty(end)){
+            queryWrapper.le("gmt_create",end);
+        }
+        teacherService.page(page,queryWrapper);
+        long total = page.getTotal();
+        List<EduTeacher> records = page.getRecords();
+        return R.ok().data("total",total).data("rows",records);
+    }
+
+    @PostMapping("addTeacher")
+    public R addTeacher(@RequestBody EduTeacher eduTeacher){
+        boolean save = teacherService.save(eduTeacher);
+        if(save) return R.ok();
+        else return R.error();
+    }
+
+    @GetMapping("getTeacher/{id}")
+    public R getTeacher(@PathVariable String id){
+        EduTeacher teacher = teacherService.getById(id);
+        return R.ok().data("teacher",teacher);
+    }
+
+    @PostMapping("updateTeacher")
+    public R updateTeacher(@RequestBody EduTeacher eduTeacher){
+        boolean update = teacherService.updateById(eduTeacher);
+        if(update) return R.ok();
+        else return R.error();
     }
 }
 
